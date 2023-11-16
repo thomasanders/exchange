691eaa0a11a26e150a9a00bb0865718a06aaaa8f
Thomas Anderson <andrzej.krzysztof.hryniewicz@gmail.com>
2023-11-16 23:59:00 +0200

    const added

--- js/script.js
+++ js/script.js
@@ -1,32 +1,36 @@
-let formElement = document.querySelector(".js-form");
-let amountElement = document.querySelector(".js-amount");
-let currencyElement = document.querySelector(".js-currency");
-let resultElement = document.querySelector(".js-result");
+{
+  const formElement = document.querySelector(".js-form");
+  const amountElement = document.querySelector(".js-amount");
+  const currencyElement = document.querySelector(".js-currency");
+  const resultElement = document.querySelector(".js-result");
 
-let rateEUR = 4.3614;
-let rateGBP = 4.9925;
-let rateUSD = 4.0079;
+  const rateEUR = 4.3614;
+  const rateGBP = 4.9925;
+  const rateUSD = 4.0079;
 
-formElement.addEventListener("submit", (event) => {
-  event.preventDefault();
+  formElement.addEventListener("submit", (event) => {
+    event.preventDefault();
 
-  let amount = +amountElement.value;
-  let currency = currencyElement.value;
+    const amount = +amountElement.value;
+    const currency = currencyElement.value;
 
-  let result;
+    let result;
 
-  switch (currency) {
-    case "EUR":
-      result = amount / rateEUR;
-      break;
-    case "USD":
-      result = amount / rateUSD;
-      break;
+    switch (currency) {
+      case "EUR":
+        result = amount / rateEUR;
+        break;
+      case "USD":
+        result = amount / rateUSD;
+        break;
 
-    case "GBP":
-      result = amount / rateGBP;
-      break;
-  }
+      case "GBP":
+        result = amount / rateGBP;
+        break;
+    }
 
-  resultElement.innerHTML = `${amount.toFixed(3)}  PLN = <strong>${result.toFixed(3)} ${currency}</strong>`;
-});
+    resultElement.innerHTML = `${amount.toFixed(
+      3
+    )}  PLN = <strong>${result.toFixed(3)} ${currency}</strong>`;
+  });
+}
